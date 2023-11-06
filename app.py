53b3474ac1fb136e6185e7b8501a3ebf059154d6
Trinity Dennis <trinity.dennis@mooreintegrity.com>
2023-11-06 17:04:55 -0600

    Added html and css to test

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    # this would be the websites home page
+    return render_template("index.html")
 
 @app.route('/shopping-cart')
 def shopping_cart():
-    return 'Use Jinja templates to build out the shopping cart UI found at this route (http://127.0.0.1:5000/shopping-cart)'
+    return 'Use templates to build out the shopping cart UI found at this route (http://127.0.0.1:5000/shopping-cart)'
 
 
 if __name__ == '__main__':
