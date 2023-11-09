b7c9adf579460e49331da924093b2d136795947e
Keenan <116980114+KayoCodes@users.noreply.github.com>
2023-11-09 01:46:29 -0600

    Front end improvements

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     # this would be the websites home page
-    return render_template("index.html")
+    return render_template("index.html", genre_page="Check Out Our Vast Selection Of Books!")
+@app.route('/Non-Fiction')
+def non_fiction():  # put application's code here
+    # test to see if i could route linke
+    #rerenders index.html which inherits from base.html. the index.html is passed a value for the genre_page variable and shown on screen when index.html is rendered
+    return render_template("index.html", genre_page="Check Out Our Non Fiction!")
+@app.route('/Fiction')
+def fiction():  # put application's code here
+    # test to see if i could route linke
+    return render_template("index.html", genre_page="Check Out Our Fiction")
+@app.route('/Romance')
+def romance():  # put application's code here
+    # test to see if i could route linke
+    return render_template("index.html", genre_page="Check Out Our  Romance")
 
 @app.route('/shopping-cart')
 def shopping_cart():
@@ -14,4 +27,4 @@ def shopping_cart():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
