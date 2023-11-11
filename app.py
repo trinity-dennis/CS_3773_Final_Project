31675f5e41d1d5091e248ee162b58d0bf092728d
Keenan <keenan.ray@my.utsa.edu>
2023-11-10 16:31:09 -0800

    Hardcoded books into a list (app.py)

--- app.py
+++ app.py
@@ -3,48 +3,68 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+#test class for books
+class books:
+    def __init__(self, name,author,img_name,price):
+        self.name = name
+        self.author = author
+        self.img = img_name
+        self.price = price
+
+books_list =[]
+loc = "assets/books/"
+books_list.append(books("Harry Potter","JK Rowling", loc+"harrypotter.jpg",5.00))
+books_list.append(books("Game of Thrones","George R Martin ", loc+"gameofthrones.jpg",5.00))
+books_list.append(books("Lightning Thief","Rick Riordan",loc+ "lightningthief.jpg",15.00))
+books_list.append(books("Shadow Of Night","Debora Harkness ",loc+ "shadowofthenight.jpg",18.81))
+books_list.append(books("Enemy Tribe (Ancestors Saga Book 3)","Lori Holmes",loc+ "enemytribe.jpg",15.99))
+books_list.append(books("The Ever King","L.J Andrews",loc+ "theeverking.jpg",17.99))
+books_list.append(books("The Witcher Tower of Swallows ","Andrzej Sapkowski",loc+ "thewitcher.jpg",14.99))
+books_list.append(books("Vows and Ruins The Legends of Thezmar","Helen Scheuerer",loc+ "vowsandruins.jpg",15.99))
+books_list.append(books("The Forbidden (Ancestors Saga Book 1)","Lori Holmes",loc+ "theforbidden.jpg",9.99))
+books_list.append(books("Daughter of Ninmah (Ancestors Saga Book 2) ","Lori Holmes",loc+ "daughterofninmah.jpg",14.99))
+books_list.append(books("The Last Kamaali (Ancestors Saga Book 4) ","Lori Holmes",loc+ "thelastkamaali.jpg",15.99))
+
 @app.route('/')
 def home_page():
     # this would be the websites home page
-    return render_template("index.html", genre_page="Check Out Our Vast Selection Of Books!")
-@app.route('/non-fiction')
-def non_fiction():
+    return render_template("index.html", genre_page="Check Out Our Vast Selection Of Books!", booksL=books_list)
     # test to see if i could route linke
     #rerenders index.html which inherits from base.html. the index.html is passed a value for the genre_page variable and shown on screen when index.html is rendered
     return render_template("index.html", genre_page="Non-Fiction")
 @app.route('/fiction')
 def fiction():
     # test to see if i could route linke
-    return render_template("index.html", genre_page="Fiction")
+    return render_template("index.html", genre_page="Fiction" ,booksL=books_list)
 @app.route('/romance')
 def romance():
     # test to see if i could route linke
-    return render_template("index.html", genre_page="Romance")
+    return render_template("index.html", genre_page="Romance",booksL=books_list)
 
 @app.route('/action')
 def action():
     # test to see if i could route linke
-    return render_template("index.html", genre_page="Action")
+    return render_template("index.html", genre_page="Action",booksL=books_list)
 
 @app.route('/comedy')
 def comedy():
     # test to see if i could route linke
-    return render_template("index.html", genre_page="Comedy")
+    return render_template("index.html", genre_page="Comedy",booksL=books_list)
 
 @app.route('/fantasy')
 def fantasy():
     # test to see if i could route linke
-    return render_template("index.html", genre_page="Fantasy")
+    return render_template("index.html", genre_page="Fantasy",booksL=books_list)
 
 @app.route('/horror')
 def horror():
     # test to see if i could route linke
-    return render_template("index.html", genre_page="Horror")
+    return render_template("index.html", genre_page="Horror",booksL=books_list)
 
 @app.route('/comic-manga')
 def comic_manga():
     # test to see if i could route linke
-    return render_template("index.html", genre_page="Comic/Manga")
+    return render_template("index.html", genre_page="Comic/Manga",booksL=books_list)
 
 @app.route('/accessories')
 def accesories():
