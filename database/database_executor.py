8d070180be3070f940912f5e3c14e10d9a920b9a
haileyjessalyn <haileyjessalyn@gmail.com>
2023-11-18 17:56:01 -0600

    make small change. search bar can now look up partial title

--- database/database_executor.py
+++ database/database_executor.py
@@ -44,7 +44,7 @@ class DatabaseExecutor:
 
     def get_books_by_title(self, title):
         with self._db_session.session() as session:
-            books = session.query(Book).filter_by(title=title.title()).all()
+            books = session.query(Book).filter(Book.title.contains(title)).all()
             for book in books:
                 session.expunge(book)
                 make_transient(book)
@@ -60,7 +60,7 @@ class DatabaseExecutor:
 
     def get_accessories_by_item_name(self, item):
         with self._db_session.session() as session:
-            accessories = session.query(Accessories).filter(Accessories.item_name.like(item)).all()
+            accessories = session.query(Accessories).filter(Accessories.item_name.contains(item)).all()
             for accessory in accessories:
                 session.expunge(accessory)
                 make_transient(accessory)
